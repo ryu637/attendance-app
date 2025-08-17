d93f6b10a2350a052f322f38b46e2aab7d28b995
ujibayashi ryunosuke <>
2025-08-18 02:11:43 +0900

    readme 加加

--- src/vite.config.js
+++ src/vite.config.js
@@ -12,6 +12,21 @@
 //   ],
 // });
 
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       'vue': 'vue/dist/vue.esm-bundler.js',
+//       '@': path.resolve(__dirname, 'resources/js'),
+//     },
+//   },
+// });
+
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
@@ -20,9 +35,11 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      'vue': 'vue/dist/vue.esm-bundler.js',
+      'vue': 'vue/dist/vue.esm-bundler.js', // ← ここ重要
       '@': path.resolve(__dirname, 'resources/js'),
     },
   },
 });
+
+
 
